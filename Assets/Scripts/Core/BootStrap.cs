de925df9f2dee0654ac30d6c7e41187effd387b9
Priyanka-16-hub <ajpriyanka16@outlook.com>
2026-09-21 15:04:18 +0530

    Implement manager startup order
    Changes
    - Added all 9 manager classes
    - Added Initialise implementation to each manager
    - Wired manager startup order according to GDD §46
    - MainLevel loads after all managers initialise
    - Verified Boot → MainLevel flow with no console errors
    
    Verification
    All 9 managers initialise in the required order:
    1. MGR_Audio
    2. MGR_Pool
    3. MGR_Inventory
    4. MGR_Save
    5. MGR_Game
    6. MGR_Order
    7. MGR_Delivery
    8. MGR_Truck
    9. MGR_UI
    
    MainLevel loads after initialization.

--- Assets/Scripts/Core/BootStrap.cs
+++ Assets/Scripts/Core/BootStrap.cs
@@ -3,30 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class Bootstrap : MonoBehaviour
 {
-    [SerializeField] private MonoBehaviour[] managerComponents;
+    [SerializeField] private MGR_Audio audioManager;
+    [SerializeField] private MGR_Pool poolManager;
+    [SerializeField] private MGR_Inventory inventoryManager;
+    [SerializeField] private MGR_Save saveManager;
+    [SerializeField] private MGR_Game gameManager;
+    [SerializeField] private MGR_Order orderManager;
+    [SerializeField] private MGR_Delivery deliveryManager;
+    [SerializeField] private MGR_Truck truckManager;
+    [SerializeField] private MGR_UI uiManager;
 
     private void Start()
     {
         InitialiseManagers();
-        LoadMainLevel();
+
+        SceneManager.LoadScene("Main_level");
     }
 
     private void InitialiseManagers()
     {
-        foreach (MonoBehaviour component in managerComponents)
-        {
-            if (component == null)
-                continue;
-
-            if (component is IManager manager)
-            {
-                manager.Initialise();
-            }
-        }
-    }
-
-    private void LoadMainLevel()
-    {
-        SceneManager.LoadScene("Main_level");
+        audioManager.Initialise();
+        poolManager.Initialise();
+        inventoryManager.Initialise();
+        saveManager.Initialise();
+        gameManager.Initialise();
+        orderManager.Initialise();
+        deliveryManager.Initialise();
+        truckManager.Initialise();
+        uiManager.Initialise();
     }
 }
---
+++ Assets/Scripts/Managers/MGR_Inventory.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class MGR_Inventory : Manager<MGR_Inventory>
+{
+    protected override void OnInitialise()
+    {
+        Debug.Log("3. MGR_Inventory initialised.");
+    }
+}
